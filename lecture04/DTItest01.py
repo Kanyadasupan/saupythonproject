81b8c8cd26fd8b358822a66c2b4c0fa749632050
Kanyadasupan <s6652410028@sau.ac.th>
2023-08-29 11:54:27 +0700

    codeV.4

--- lecture04/DTItest01.py
+++ lecture04/DTItest01.py
@@ -6,10 +6,11 @@ front_back = float(width * long * 2 )
 left_rigth = float(width * high * 2 )
 top_bottom = float(width * width * 2 ) 
 total_area = float(front_back + left_rigth + top_bottom ) / 5
-a = round(total_area)
+t = round(total_area)
 
-print("พื้นที่ทั้งหมด",a,"Cm2")
-
-
+print("พื้นที่ทั้งหมด",t,"Cm2")
+print("พื้นที่ทั้งหมด" + " " + str(t) + " " + "Cm2")
+print("พื้นที่ทั้งหมด {:.2f} Cm2".format(t))
+print(f"พื้นที่ทั้งหมด {t:.2f} Cm2 ")
 
 
