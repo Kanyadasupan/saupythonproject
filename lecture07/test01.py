2aa8947f7a6c5a1274c50b4bbec007a9fb7c5ed5
Kanyadasupan <142291069+Kanyadasupan@users.noreply.github.com>
2023-10-10 12:33:34 +0700

    Add files via upload

---
+++ lecture07/test01.py
@@ -0,0 +1,24 @@
+# variable
+score = 1234
+stu_name = "som"
+flag = True
+# ++++++++++++++++++
+# List
+    #   0   1       2       3       4           5
+var1 = [10, 20, "Hello" , True, [111,"wow"],"sumsum"]
+    #   -6  -5      -4      -3      -2          -1
+
+print(var1[0] + var1[1])
+print(var1[-6] + var1[-5])
+print(var1[0] + var1[4][0])
+print(var1[-6] + var1[-2][-2])
+
+# Tuple
+    #   0   1       2       3       4           5
+var2 = (10, 20, "Hello" , True, (111,"wow"),"sumsum")
+    #   -6  -5      -4      -3      -2          -1
+
+print(var2[0] + var2[1])
+print(var2[-6] + var2[-5])
+print(var2[-6] + var2[-2][-2])
+print(f'{var2[2]}...{var2[5]} {var2[4][1]}...') #Hello...som Wow...
